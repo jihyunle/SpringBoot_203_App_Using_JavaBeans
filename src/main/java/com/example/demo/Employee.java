ca4a0db53a08d90e07b1d16cde69addddee19a39
jihyunle <jennifer.jlee@outlook.com>
2019-02-26 10:31:05 -0500

    Initial commit

---
+++ src/main/java/com/example/demo/Employee.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private String dob;
+
+    public Employee(){
+
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSsn() {
+        return ssn.substring(0,3) + "-" + ssn.substring(3,5) + "-" + ssn.substring(5);
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+}
---
+++ src/main/java/com/example/demo/SpringBoot203AppUsingJavaBeansApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot203AppUsingJavaBeansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot203AppUsingJavaBeansApplication.class, args);
+    }
+
+}
